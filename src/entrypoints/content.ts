3d29d3ec147abf92b1bf060e6649e83068181a31
Ryoya Tamura <ryoya.tamura@nulab.com>
2025-11-11 03:25:54 +0900

    feat: add styling for tooltip and enhance content script functionality

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,9 +1,27 @@
 import { defineContentScript } from "wxt/utils/define-content-script";
+import "../assets/style.css";
 
 export default defineContentScript({
 	matches: ["<all_urls>"],
 	allFrames: true,
 	main() {
-		console.log("Content script loaded");
+		setTimeout(() => {
+			for (const a of document.getElementsByTagName("a")) {
+				try {
+					const url = new URL(a.href);
+
+					if (
+						url.hostname.endsWith("backlog.jp") ||
+						url.hostname.endsWith("backlog.com")
+					) {
+						if (url.href === a.textContent) {
+							a.textContent = "Hello.";
+						}
+					}
+				} catch {
+					// ignore invalid URL
+				}
+			}
+		}, 3000);
 	},
 });
